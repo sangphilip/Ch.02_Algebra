c93d3e29627fb00751a94298d7fe066acb18dda1
psang <sangphilip1@gmail.com>
2021-03-17 13:05:29 -0400

    Changed name

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,23 +1,26 @@
 '''
-Sign your name:________________
+Sign your name:Phils
 
 1.) How do you enter a single line comment in a program? Give an example.
-
+#By doing this
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
+'''
+a = 2
+b = 5
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+print(b / a)
+print(b // a)
+print(b ** a)
+print(b % a)
+print(a + b)
+print(type(42))
+print(type(42.0))
+print(type("C3PO"))
+print(type(True))
 
-
+'''
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
 into the Python Console Window?
 
